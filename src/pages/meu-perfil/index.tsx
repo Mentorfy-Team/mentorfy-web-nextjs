bcdd15eeec7cf8172fd5472487e2cf2853495bea
Henrique Francisco <henriquesouza432@outlook.com>
2022-09-01 19:04:08 -0300

    wip: created the my profile page and started the styles

---
+++ src/pages/meu-perfil/index.tsx
@@ -0,0 +1,6 @@
+import { default as  MyProfileLayout } from '~/layouts/myprofile';
+import { getProps as getProps } from '~/layouts/myprofile';
+
+export default MyProfileLayout;
+
+export const getStaticProps = getProps;
