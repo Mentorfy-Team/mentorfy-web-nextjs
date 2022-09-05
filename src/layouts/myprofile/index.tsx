c162d1f3c74d31582860ab0cb03648bbca1edc5b
Henrique Francisco <henriquesouza432@outlook.com>
2022-09-05 10:16:55 -0300

    wip: ajusted some styles

--- src/layouts/myprofile/index.tsx
+++ src/layouts/myprofile/index.tsx
@@ -1,65 +1,70 @@
 import { FC } from 'react';
-import { Avatar, Button, Typography } from '@mui/material';
+import { Avatar } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 
-import { Form, Header, InputField, TextWrapper, Wrapper, WrapperContent } from './style';
+import { AvatarWrapper, ButtonInputWrapper, Buttons, CustomTypography, Form, FormContentWrapper, Header, InputField, TextWrapper, Wrapper, WrapperContent } from './style';
 
 const MyProfile: FC = () => {
     const theme = useTheme();
     return (
         <Wrapper>
             <Header>
-                <Typography variant='h5'
-                >Meu Perfil</Typography>
+                <CustomTypography sx={{fontWeight: 600, fontSize: '1.5rem'}}>Meu Perfil</CustomTypography>
             </Header>
             <WrapperContent>
                 <TextWrapper>
-                    <Typography variant='h6'>Dados Gerais</Typography>
-                    <Typography>informações sobre a sua conta</Typography>
+                    <CustomTypography variant='h6' >Dados Gerais</CustomTypography>
+                    <CustomTypography sx={{ fontWeight: 'lighter', fontSize: '0.95rem'}}>Informações sobre a sua conta</CustomTypography>
                 </TextWrapper>
                 <Form>
-                    <Typography>Foto</Typography>
-                    <Avatar alt='foto-perfil' src='/images/avatar.png' />
-                    <Button variant='outlined'>Alterar</Button>
-
-                    <InputField
-                    label='Nome'
-                    type='text'
-                    placeholder='Digite seu nome'
-                    InputLabelProps={{
-                        shrink: true,
-                      }}
-                      />
-
-                    <InputField
-                    variant='standard'
-                    label='E-mail'
-                    type='e-mail'
-                    placeholder='Digite seu e-mail'
-                    InputLabelProps={{
-                        shrink: true,
-                      }}
-                      />
-                     <Button variant='text'>Alterar e-mail</Button>
-                    <InputField
-                    label='Telefone'
-                    type='tel'
-                    placeholder='Digite seu telefone'
-                    InputLabelProps={{
-                        shrink: true,
-                      }}
-                      />
-                    <InputField
-                    label='Senha'
-                    type='password'
-                    placeholder='********'
-                    InputLabelProps={{
-                        shrink: true,
-                      }}
-                      />
-                    <Button variant='outlined'>Alterar senha</Button>
-
-                    <Button variant='contained'>Salvar alterações</Button>
+                    <FormContentWrapper>
+                        <CustomTypography>Foto</CustomTypography>
+                        <AvatarWrapper>
+                            <Avatar
+                            alt='foto-perfil'
+                            src='/images/avatar.png'/>
+                            <Buttons variant='outlined'>Alterar</Buttons>
+                        </AvatarWrapper>
+                        <InputField
+                        label='Nome'
+                        type='text'
+                        placeholder='Digite seu nome'
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        />
+                        <ButtonInputWrapper>
+                            <InputField
+                            label='E-mail'
+                            type='e-mail'
+                            placeholder='Digite seu e-mail'
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            />
+                            <Buttons variant='text'>Alterar e-mail</Buttons>
+                        </ButtonInputWrapper>
+                        <InputField
+                        label='Telefone'
+                        type='tel'
+                        placeholder='Digite seu telefone'
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        />
+                        <ButtonInputWrapper>
+                            <InputField
+                            label='Senha'
+                            type='password'
+                            placeholder='********'
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            />
+                            <Buttons variant='outlined'>Alterar senha</Buttons>
+                        </ButtonInputWrapper>
+                    </FormContentWrapper>
+                    <Buttons variant='contained'type='submit' className='submit-button'>Salvar alterações</Buttons>
                 </Form>
             </WrapperContent>
 
--- src/layouts/myprofile/style.ts
+++ src/layouts/myprofile/style.ts
@@ -1,4 +1,4 @@
-import { Box, TextField, styled } from '@mui/material';
+import { Box, Button, TextField, Typography, styled  } from '@mui/material';
 
 export const Wrapper = styled('div')`
     background-color:  ${({ theme }) => theme.palette.primary.light};
@@ -6,6 +6,7 @@ export const Wrapper = styled('div')`
     flex-direction: column;
 
     gap: 1.5rem;
+    height: 100vh; 
     padding: 0.625rem 1.5rem ; 
 `;
 
@@ -14,23 +15,91 @@ export const Header = styled('header')`
 
 export const WrapperContent = styled(Box)`
     display: flex;
+
+    @media (max-width: 500px) { 
+        flex-direction: column;
+    }
 `;
 
 export const Form = styled('form')`
-    background-color: ${ ({theme}) => theme.palette.primary.main};
-    border-radius: 0.5rem;
-    
     display: flex;
     flex-direction: column;
-    padding: 0.625rem 1.5rem ;
-    width: 60%;
+    width: 70%;
+    .submit-button { 
+        align-self: flex-end;
+        margin-top: 1.5rem;
+    }
+
+    @media (max-width: 500px) { 
+        width:100%;
+        
+    }  
+`;
+
+export const FormContentWrapper = styled(Box)`
+    align-items: flex-start;
+    background-color: ${ ({theme}) => theme.palette.primary.main};
     
+    border-radius: 0.5rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1.7rem;
+    height: 75vh;
+    padding: 1.5rem;
 `;
 
 export const InputField = styled(TextField)`
+    color: ${({theme}) => theme.palette.text.primary};
+    height: 10px;
+    
+    width: 50%;
+    
+    input { 
+        height: 0.625rem;
+
+    }
+    label { 
+        color: ${({theme}) => theme.palette.text.primary};
+    }
+    fieldset  { 
+        border-color: ${({theme}) => theme.palette.text.primary} ;
+        height: 2.8125rem;
+    }
 `;
 
 export const TextWrapper = styled('div')`
-    width: 40%;
+    width: 30%;
+
+    @media (max-width: 500px) { 
+        width:100%;
+        margin-bottom: 1rem;
+    }
 `;
 
+export const CustomTypography = styled(Typography)`
+    color: ${({theme}) => theme.palette.text.primary};
+`;
+
+export const AvatarWrapper = styled('div')`
+    align-items: center;
+    display: flex;
+    gap: 1rem;
+
+    justify-content: center;
+    margin-top: -1rem;
+`;
+
+export const Buttons = styled(Button)`
+    height: 2rem;
+    text-transform: none;
+`;
+
+export const ButtonInputWrapper = styled('div')`
+    display:flex;
+    
+    gap: 1rem;
+    
+    margin: 3rem 0;
+    width: 100%;
+   
+`;
