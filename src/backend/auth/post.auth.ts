09b80415943ee71adc64047cb091cacf48d9ee29
Alan Jones Rios <alan.jones.rios@gmail.com>
2022-09-12 19:56:43 -0300

    some fixing in authentication

--- src/backend/auth/post.auth.ts
+++ src/backend/auth/post.auth.ts
@@ -1,17 +1,15 @@
 import { supabase } from '~/backend/supabase';
-import { DataUtil } from '~/shared/utils';
 type Request = UsersApi.Post.Request;
 type Response = UsersApi.Post.Response;
 
 export const post: Handler.Callback<Request, Response> = async (req, res) => {
   const { email, password } = req.body;
+  await new Promise((resolve) => setTimeout(resolve, 1000));
 
   const { session, user } = await supabase.auth.signIn({
     email,
     password,
   });
-
-  await new Promise((resolve) => setTimeout(resolve, 1000));
 
   res.status(200).json({
     session: {
--- src/layouts/acesso/index.tsx
+++ src/layouts/acesso/index.tsx
@@ -4,7 +4,6 @@ import { InferGetStaticPropsType } from 'next';
 import Image from 'next/future/image';
 import { useRouter } from 'next/router';
 import PageWrapper from '~/components/partials/PageWrapper';
-import { RecoveryProps } from '~/pages/_app';
 import { handleAcessoSubPage } from './helper/SwitchSubPages';
 import { AlignSelf, BackgroundHolder, Grid, Wrapper } from './styles';
 import backgroundImage from '~/../public/images/background-login.jpg';
@@ -17,20 +16,6 @@ export type AcessoSubPage =
   | 'esqueci-minha-senha'
   | 'sucesso'
   | 'confirmar-conta';
-
-const keyStr =
-  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-
-const triplet = (e1: number, e2: number, e3: number) =>
-  keyStr.charAt(e1 >> 2) +
-  keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
-  keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
-  keyStr.charAt(e3 & 63);
-
-const rgbDataURL = (r: number, g: number, b: number) =>
-  `data:image/gif;base64,R0lGODlhAQABAPAA${
-    triplet(0, r, g) + triplet(b, 255, 255)
-  }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`;
 
 function LoginView(props: InferGetStaticPropsType<typeof getProps>) {
   const mobile = useMediaQuery('(max-width:500px)');
