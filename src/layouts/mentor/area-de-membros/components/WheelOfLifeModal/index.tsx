77f2e7753a6db23c026ae7b18b44fb7fcf4efecc
Henrique Francisco <henriquesouza432@outlook.com>
2022-09-29 13:46:11 -0300

    fix: fixed the wheel of life areas list

--- src/layouts/mentor/area-de-membros/components/WheelOfLifeModal/index.tsx
+++ src/layouts/mentor/area-de-membros/components/WheelOfLifeModal/index.tsx
@@ -2,19 +2,91 @@ import { useState } from 'react';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Box from '@mui/material/Box';
+import Checkbox, { CheckboxProps } from '@mui/material/Checkbox';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
+import { useTheme } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import DescriptionInputField from '~/components/atoms/DescriptionInputField';
 import InputField from '~/components/atoms/InputField';
 import ModalComponent from '~/components/modules/Modal';
-import { ContentBox } from './styles';
+import { AreasBox, ContentBox } from './styles';
 
 const WheelOfLife = () => {
     const [open, setOpen] = useState(false);
+    const theme = useTheme();
 
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
+
+    const WheelAreas = [
+        {
+            id: 0,
+            name: 'CONTRIBUIÇÃO SOCIAL'
+        },
+        {
+            id: 0,
+            name: 'DESENVOLVMENTO INTELECTUAL'
+        },
+        {
+            id: 0,
+            name: 'EQUILÍBRIO EMOCIONAL'
+        },
+        {
+            id: 0,
+            name: 'ESPIRITUALIDADE'
+        },
+        {
+            id: 0,
+            name: 'FAMÍLIA'
+        },
+        {
+            id: 0,
+            name: 'LAZER E HOBBIES'
+        },
+        {
+            id: 0,
+            name: 'PLENITUDE FINANCEIRA'
+        },
+        {
+            id: 0,
+            name: 'REALIZAÇÃO DE PROPÓSITO'
+        },
+        {
+            id: 0,
+            name: 'RELACIONAMENTO AMOROSO'
+        },
+        {
+            id: 0,
+            name: 'SAÚDE E CONDIÇÃO FÍSICA'
+        },
+        {
+            id: 0,
+            name: 'SAÚDE FINANCEIRA'
+        },
+        {
+            id: 0,
+            name: 'VIDA SOCIAL E AMIZADE'
+        },
+    ];
+
+    function BpCheckbox(props: CheckboxProps) {
+        return (
+            <Checkbox
+                sx={{
+                    padding: '0',
+                    color: `${theme.palette.caption.dark}`,
+                    '& .MuiSvgIcon-root': { fontSize: 18 },
+                    '&.Mui-checked': {
+                        color: 'green',
+                    },
+                }}
+                disableRipple
+                color="default"
+                {...props}
+            />
+        );
+    }
 
     return (
         <ModalComponent title='Roda da Vida'>
@@ -22,17 +94,25 @@ const WheelOfLife = () => {
                 <InputField label='Título' placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit." />
                 <DescriptionInputField label='Descrição' placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum facilisis in lobortis orci aliquet. In nisl elit sodales morbi euismod ullamcorper egestas aenean amet. Gravida penatibus massa, duis felis. Vitae, pellentesque eget nunc facilisi in dictumst. Malesuada sed condimentum viverra vel pellentesque magna." />
                 <ContentBox>
-                <Box sx={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
-                        {open ? <IconButton sx={{ color: 'gray', width: '40px', height: '40px' }} onClick={handleClose}>
-                                 <ExpandMoreIcon />
-                                </IconButton>
-                              :
-                                <IconButton sx={{ color: 'gray', width: '40px', height: '40px' }}  onClick={handleOpen}>
-                                    <ArrowForwardIosIcon sx={{fontSize: 'medium'}}/>
+                    <>
+                        <Box sx={{ display: 'flex', alignItems: 'center', gap: '0.1rem' }}>
+                            {open ? <IconButton sx={{ color: 'gray', width: '40px', height: '40px' }} onClick={handleClose}>
+                                <ExpandMoreIcon />
+                            </IconButton>
+                                :
+                                <IconButton sx={{ color: 'gray', width: '40px', height: '40px' }} onClick={handleOpen}>
+                                    <ArrowForwardIosIcon sx={{ fontSize: 'medium' }} />
                                 </IconButton>}
-                        <Typography sx={{ fontSize: '1rem', fontWeight: '600' }}>Áreas da Roda da Vida</Typography>
-                    </Box>
-                    <Divider sx={{borderColor: '#6e6e6e55', marginBottom: '0.5rem'}}/>
+                            <Typography sx={{ fontSize: '1rem', fontWeight: '600' }}>Áreas da Roda da Vida</Typography>
+                        </Box>
+                        <Divider sx={{ borderColor: '#6e6e6e55', marginBottom: '0.5rem', width: '90%', float: 'right' }} />
+                        {WheelAreas.map((index) => (
+                            <AreasBox key={index.id}>
+                                <BpCheckbox defaultChecked/>
+                                <Typography sx={{ fontSize: '0.7rem', fontWeight: '300', opacity: '0.8' }}>{index.name}</Typography>
+                            </AreasBox>
+                        ))}
+                    </>
                 </ContentBox>
             </>
         </ModalComponent>
--- src/layouts/mentor/area-de-membros/components/WheelOfLifeModal/styles.ts
+++ src/layouts/mentor/area-de-membros/components/WheelOfLifeModal/styles.ts
@@ -6,5 +6,16 @@ background-color: ${({theme}) => theme.palette.primary.dark};
 border-radius: 5px;
 height: 227px;
 overflow: auto;
-padding: 0.7rem 1rem;
+padding: 0.7rem 0.5rem;
+`;
+
+export const AreasBox = styled(Box)`
+    align-items: center;
+    border-bottom: 1px solid #6e6e6e55;
+    display: flex;
+    float: right;
+    gap: 0.5rem;
+    margin-bottom: 0.8rem;
+    padding: 0 0 0.2rem 0;
+    width: 90%;
 `;
