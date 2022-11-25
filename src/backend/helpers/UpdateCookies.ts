4a86f067e406182688810ac6c2e7544302cb9954
Alan Jones Rios <alan.jones.rios@gmail.com>
2022-11-25 08:35:27 -0300

    fixing access_token

--- src/backend/helpers/UpdateCookies.ts
+++ src/backend/helpers/UpdateCookies.ts
@@ -2,9 +2,12 @@ import { default as cookieHelper } from 'cookie';
 
 const UpdateCookies = async (supabase, req) => {
   // get sb-storage from cookie
-  const headerCookies = req?.headers?.cookie
-    ? JSON.parse(cookieHelper.parse(req?.headers?.cookie)['sb-storage'])
-    : '';
+  const headerCookies =
+    req?.headers?.cookie && req?.headers?.cookie['supabase-auth-token']
+      ? JSON.parse(
+          cookieHelper.parse(req?.headers?.cookie)['supabase-auth-token'],
+        )
+      : '';
 
   if (headerCookies) await supabase.auth.setSession(headerCookies);
 
