53b6df567d1e4b1623dff957084f878066952ea3
Alan Jones Rios <alan.jones.rios@gmail.com>
2022-12-03 17:06:35 -0300

    fix build

--- src/backend/http/clients/index.ts
+++ src/backend/http/clients/index.ts
@@ -165,7 +165,7 @@ export const del = async (req, res) => {
   });
 };
 
-export const get: Handler.Callback<Request, Response> = async (req, res) => {
+export const get = async (req, res) => {
   await new Promise((resolve) => setTimeout(resolve, 2000));
 
   res.status(200).json({});
