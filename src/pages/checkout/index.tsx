2221570dc663012b98e3911734b9bd9eaa7c0588
Henrique Francisco <henriquesouza432@outlook.com>
2022-12-06 15:55:40 -0300

    feat: created checkout page

---
+++ src/pages/checkout/index.tsx
@@ -0,0 +1,6 @@
+import { default as CheckoutLayout } from '~/layouts/checkout';
+import { getProps as getProps } from '~/layouts/checkout';
+
+export default CheckoutLayout;
+
+export const getServerSideProps = getProps;
