1fcbabb487984c06e0f5eb6c48463f9c8f2aff01
Alan Jones Rios <alan.jones.rios@gmail.com>
2022-12-06 13:07:18 -0300

    beta access

---
+++ src/consts/beta-access.ts
@@ -0,0 +1,9 @@
+export const betaEmailAccess = [
+  'will.upstr@gmail.com',
+  'alan.jones.rios@gmail.com',
+  'workermz@gmail.com',
+  'workermzk@gmail.com',
+  'comercial@alligaredigital.com.br',
+  'alligare@gmail.com',
+  'neri.couto@gmail.com',
+];
