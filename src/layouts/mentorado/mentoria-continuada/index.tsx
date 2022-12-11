e0c489e4843afb47ce14643af4c2640c124b893a
Alan Jones Rios <alan.jones.rios@gmail.com>
2022-12-11 10:29:46 -0300

    show module conclusion image in ongoing area

--- src/layouts/mentorado/mentoria-continuada/index.tsx
+++ src/layouts/mentorado/mentoria-continuada/index.tsx
@@ -210,6 +210,30 @@ const ContinuosMentoring: FC<
                         />
                       </Task>
                     ))}
+
+                    {step.rows.filter(
+                      (task) =>
+                        userInput.findIndex(
+                          (inp) =>
+                            inp.member_area_tool_id.toString() === task.id,
+                        ) !== -1,
+                    ).length === step.rows.length &&
+                      step.rows.length > 0 && (
+                        <Image
+                          alt="imagem"
+                          width={200}
+                          height={120}
+                          src={
+                            (step.extra as any)?.length > 1
+                              ? step.extra[1].sourceUrl
+                              : '/svgs/finished.svg'
+                          }
+                          style={{
+                            marginTop: '1.2rem',
+                            objectFit: 'contain',
+                          }}
+                        />
+                      )}
                   </TasksWrapper>
                 </Bundle>
               ))}
