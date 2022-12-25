5a05aa0aa6d045c51eff4659a645b274d449ba6e
Alan Jones Rios <alan.jones.rios@gmail.com>
2022-12-25 10:09:22 -0300

    fix: added missing commit

--- src/backend/repositories/pagarme/subscription/CreateSubscription.ts
+++ src/backend/repositories/pagarme/subscription/CreateSubscription.ts
@@ -2,15 +2,24 @@ import { HttpPagarme } from '~/backend/helpers/HttpPagarme';
 
 export const CreateSubscription = async (
   data: Pagarme.Subscription.Request,
-) => {
+): Promise<
+  Pagarme.Subscription.Response & Partial<Pagarme.Subscription.DataError>
+> => {
+  const info = data;
+  info.card.billing_address = data.customer.address;
   try {
     const response = await HttpPagarme.post<Pagarme.Subscription.Response>(
       '/subscriptions',
-      data,
+      info,
     );
 
     return response.data;
   } catch (error) {
-    return null;
+    const { response } = error as Pagarme.Subscription.errors;
+    if (error) {
+      return response.data as any;
+    } else {
+      return null;
+    }
   }
 };
