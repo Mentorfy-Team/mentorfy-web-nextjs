a812fdcaeced80870e4c9b0f1d3c55ce536b3ba4
Alan Jones Rios <alan.jones.rios@gmail.com>
2022-12-28 23:56:46 -0300

    update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const next = require('next');
 const krabs = require('krabs').default;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
-
+//
 async function main() {
   try {
     await app.prepare();
