7c9ecd1dbe875858ba4177abc10e9352b5b18a6c
Alan Jones Rios <alan.jones.rios@gmail.com>
2022-12-30 20:35:44 -0300

    log lead api

--- src/backend/http/lead/approval/index.ts
+++ src/backend/http/lead/approval/index.ts
@@ -2,13 +2,13 @@ import { SupabaseServer } from '../../../supabase';
 
 export const get = async (req, res) => {
   const supabase = SupabaseServer(req, res);
-
+  console.log('lead: ', req.query.email);
   const { data: lead, error: error } = await supabase
     .from('lead_approval')
     .select('*')
     .eq('email', req.query.email)
     .eq('status', 'approved')
     .single();
-
+  console.log('lead data: ', lead, error);
   res.status(200).json(lead);
 };
